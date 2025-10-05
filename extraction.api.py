ec143b97533b3519a890fc9b12c893658e73f2b5
Samuel Freitas <samuelfreitas2998@gmail.com>
2025-10-05 11:46:07 -0300

    Utiliza outro script usando boas praticas em gestão de custo vizando o processamento

--- extraction.api.py
+++ extraction.api.py
@@ -3,19 +3,31 @@ import pandas as pd
 
 # Faz a requisição para todas as páginas da API do Rick and Morty
 url = 'https://rickandmortyapi.com/api/character'
+
+response = requests.get(url)
+data = response.json()
+
+#Inicializa uma lista para armazenar todos os personagens
 all_characters = []
 
-while url:
-    response = requests.get(url)
+# Loop for para percorrer todas as páginas
+for page in range(1,data['info']['pages']+1):
+    response = requests.get(f'{url}?page={page}')
     data = response.json()
-    all_characters.extend(data['results'])
-    url = data['info']['next']  # Próxima página ou None
+    all_characters.extend(data['results'])  # Adiciona os personagens da página atual à lista
+    print(f'Página {page} processada.')
 
+# Converte a lista de personagens em um DataFrame do pandas
 df = pd.DataFrame(all_characters)
+print(df.head())
 
-# Salva os dados em um arquivo CSV
+# Conta o número total de personagens extraídos
+df_count = len(df)
+print(f'Total de personagens extraídos: {df_count}')
+
+# Salva o DataFrame em um arquivo CSV
 df.to_csv('rick_and_morty_characters.csv', index=False)
-print("Dados dos personagens salvos em 'rick_and_morty_characters.csv'")
+print("Extração concluída e salva em 'rick_and_morty_characters.csv'")
 
 
 
